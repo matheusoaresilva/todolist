50a9f295e499225ebd5ac77687743784f9d5bfd9
Matheus Soares <matheusmatias2622@gmail.com>
2023-05-14 16:58:12 -0300

    added slidable delete

--- lib/widgets/todo_list_item.dart
+++ lib/widgets/todo_list_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:intl/intl.dart';
 
 import '../models/todo.dart';
@@ -10,28 +11,39 @@ class TodoListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        color: Colors.grey[200],
-      ),
-      margin: const EdgeInsets.symmetric(vertical: 4),
-      padding: EdgeInsets.all(16),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            DateFormat('dd/MM/yyyy - EE - HH:mm').format(todo.dateTime),
-            style: TextStyle(
-              fontSize: 12,
-            ),
+    return Slidable(
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.grey[200],
           ),
-          Text(
-            todo.title,
-            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+          margin: const EdgeInsets.symmetric(vertical: 4),
+          padding: EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                DateFormat('dd/MM/yyyy - EE - HH:mm').format(todo.dateTime),
+                style: TextStyle(
+                  fontSize: 12,
+                ),
+              ),
+              Text(
+                todo.title,
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+              ),
+            ],
           ),
+        ),
+        actionPane: const SlidableDrawerActionPane(),
+        
+        secondaryActions: [
+          IconSlideAction(
+            color: Colors.red,
+            icon: Icons.delete,
+            onTap: () {},
+          )
         ],
-      ),
-    );
+        );
   }
 }
