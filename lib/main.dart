1c7927c2f2f3ef343f0904cf01d74b353a45e119
Matheus Soares <matheusmatias2622@gmail.com>
2023-05-14 03:34:10 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:todolistproject/pages/todo_list_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: TodoListPage(),
+    );
+  }
+}
---
+++ lib/pages/todo_list_page.dart
@@ -0,0 +1,95 @@
+
+import 'package:flutter/material.dart';
+
+class TodoListPage extends StatefulWidget {
+  TodoListPage({super.key});
+
+  @override
+  State<TodoListPage> createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage> {
+  final TextEditingController todosController = TextEditingController();
+
+  List<String> todos = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: todosController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Adicione uma tarefa',
+                        hintText: 'Ex. Estudar Flutter',
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 8),
+                  ElevatedButton(
+                    onPressed: () {
+                      String text = todosController.text;
+                      setState(() {
+                        todos.add(text);
+                      });
+                      todosController.clear();
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xff038C7F),
+                      padding: const EdgeInsets.all(14), // espaçamento igual
+                    ),
+                    child: Icon(
+                      Icons.add,
+                      size: 30,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              ListView(
+                shrinkWrap: true,
+                children: [
+                  for (String todo in todos)
+                    ListTile(
+                      title: Text('$todo'),
+                      subtitle: Text('20/11/2023'),
+                    ),
+                ],
+              ),
+              SizedBox(height: 20),
+              Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      'Você possui 0 tarefas pendentes',
+                    ),
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xff038C7F),
+                      padding: const EdgeInsets.all(14), // espaçamento igual
+                    ),
+                    child: Text('Limpar tudo'),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
